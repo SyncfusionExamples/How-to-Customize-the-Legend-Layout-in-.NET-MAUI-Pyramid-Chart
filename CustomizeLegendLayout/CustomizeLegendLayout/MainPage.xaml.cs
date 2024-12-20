6da4256d1a5863306c105b7358bca4c6728ef7f5
tineshkumar.sf4652 <tineshkumar.sf4652@gmail.com>
2024-12-20 21:19:51 +0530

    Attach the KB sample

---
+++ CustomizeLegendLayout/CustomizeLegendLayout/MainPage.xaml.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Maui.Layouts;
+using Microsoft.Maui.Layouts;
+using Syncfusion.Maui.Charts;
+using System.Globalization;
+
+namespace CustomizeLegendLayout
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+    }
+
+    public class LegendExt:ChartLegend
+    {
+        protected override double GetMaximumSizeCoefficient()
+        {
+            return 0.7;
+        }
+    }
+
+    public class UniformLayouts : UniformItemsLayout
+    {
+
+    }
+
+}
---
+++ CustomizeLegendLayout/CustomizeLegendLayout/MauiProgram.cs
@@ -0,0 +1,29 @@
+﻿using CommunityToolkit.Maui;
+using Microsoft.Extensions.Logging;
+using Syncfusion.Maui.Core.Hosting;
+
+namespace CustomizeLegendLayout
+{
+    public static class MauiProgram
+    {
+        public static MauiApp CreateMauiApp()
+        {
+            var builder = MauiApp.CreateBuilder();
+            builder.ConfigureSyncfusionCore();
+            builder.UseMauiCommunityToolkit();
+            builder
+                .UseMauiApp<App>()
+                .ConfigureFonts(fonts =>
+                {
+                    fonts.AddFont("OpenSans-Regular.ttf", "OpenSansRegular");
+                    fonts.AddFont("OpenSans-Semibold.ttf", "OpenSansSemibold");
+                });
+
+#if DEBUG
+    		builder.Logging.AddDebug();
+#endif
+
+            return builder.Build();
+        }
+    }
+}
